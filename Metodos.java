6b24a790c0ffc126de016192bb9a04f25ef3ea57
Francisco Sánchez <81460472+pakosanchez27@users.noreply.github.com>
2022-10-20 18:16:06 -0500

    Add: Actividad

--- Metodos.java
+++ Metodos.java
@@ -1,18 +1,61 @@
-package Actividad_2;
+package Actividad3;
 
 public class Metodos {
-    int A,B, mayor = 0;
-    void Asignar (int a){
-        A = a;
+
+    double suma = 0, total, descuento,mensualidad=0;
+    void Asignar (double a){
+        suma = suma + a;
     }
 
-    void operacion(){
-        if (A > mayor){
-            mayor = A;
+    double mostrar() {
+        return suma;
+    }
+
+    double ValidacioEfectivo() {
+        if (suma <= 2000 || suma <= 4000) {
+            descuento = suma * .10;
+            total = suma - descuento;
+        } else if (suma > 4000 || suma <= 6000 ) {
+            descuento = suma * .20;
+            total = suma - descuento;
+        } else if (suma > 6000 ) {
+            descuento = suma * .30;
+            total = suma - descuento;
         }
+        return total;
+    }
+    double ValidarTDC(){
+        descuento = suma * .25;
+        return descuento;
+    }
+    double ValidarDep(){
+        descuento = suma * .30;
+        return descuento;
+    }
+    void mesesTDC(int b){
+
+        if(b == 1){
+            mensualidad = suma / 6;
+        }else if(b == 2) {
+            mensualidad = suma / 12;
+        }
+
     }
 
-    int respuesta (){
-        return mayor;
+    void mesesDep(int b){
+
+        if(b == 1){
+            mensualidad = suma / 12;
+        }else if(b == 2) {
+            mensualidad = suma / 18;
+        }
+
+    }
+    double mostrarMeses(){
+        return mensualidad;
     }
 }
+
+
+
+
--- Principal.java
+++ Principal.java
@@ -1,31 +1,62 @@
-package Actividad_2;
+package Actividad3;
 
 import java.util.Scanner;
 
 public class Principal {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Metodos metodo = new Metodos();
-        int valor, res2;
+        int res,res2,res3;
         do {
-            for (int i = 1; i <= 5; i++) {
-                System.out.println("Ingresa el valor " + i);
-                valor = scan.nextInt();
-                metodo.Asignar(valor);
-                metodo.operacion();
-            }
-            System.out.println("El Numero mayor es: " + metodo.respuesta());
 
 
-            System.out.println("¿Deseas realizar una nueva consulta?");
-            System.out.println("1.Si");
-            System.out.println("2.No");
-            res2 = scan.nextInt();
-        }while (res2 == 1);
+        double producto;
+        for (int i  = 1;  i <= 4; i++){
+            System.out.println("Ingresa el precio del producto " + i);
+            producto = scan.nextDouble();
+            metodo.Asignar(producto);
         }
+        System.out.println("Tu total es: " + metodo.mostrar());
+        System.out.println("Escoje tu Metodo de pago");
+        System.out.println("1. Paga en efectivo");
+        System.out.println("2. Paga con TDC");
+        System.out.println("3. Pago con tarjeta departamental");
+        res = scan.nextInt();
+        switch (res){
+            case 1:
+                System.out.println("Pago en Efectivo ");
+                System.out.println("Tu Total es : " + metodo.ValidacioEfectivo());
+                break;
+            case 2:
 
-
-
-
+                System.out.println("Pago con TDC");
+                System.out.println("Elige los meses a Diferir");
+                System.out.println("1. 6 meses sin intereses");
+                System.out.println("2. 12 meses sin intereses");
+                res2 = scan.nextInt();
+                metodo.mesesTDC(res2);
+                System.out.println("tu pago mensual sera de: " + metodo.mostrarMeses());
+                System.out.println("Tienes un 25% de descuento en dinero electronico");
+                System.out.println("Tu total es de : " +metodo.mostrar());
+                System.out.println("Tu nuevo saldo en dinero electronico es: " + metodo.ValidarTDC());
+                break;
+            case 3:
+                System.out.println("Pago con TDC");
+                System.out.println("Elige los meses a Diferir");
+                System.out.println("1. 12 meses sin intereses");
+                System.out.println("2. 18 meses sin intereses");
+                res2 = scan.nextInt();
+                metodo.mesesDep(res2);
+                System.out.println("tu pago mensual sera de: " + metodo.mostrarMeses());
+                System.out.println("Tienes un 30% de descuento en dinero electronico");
+                System.out.println("Tu total es de : " +metodo.mostrar());
+                System.out.println("Tu nuevo saldo en dinero electronico es: " + metodo.ValidarDep());
+                break;
+        }
+            System.out.println("¿quieres registrar mas productos");
+            System.out.println("1. si");
+            System.out.println("2. no");
+            res3=scan.nextInt();
+    }while (res3 == 1);
+}
 }
