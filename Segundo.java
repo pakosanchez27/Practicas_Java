389e7d241efdf943349cefae373e09be4e2a74c4
Francisco Sánchez <81460472+pakosanchez27@users.noreply.github.com>
2022-12-01 18:05:26 -0600

    Add files via upload

--- Segundo.java
+++ Segundo.java
@@ -1,9 +1,16 @@
 package examen;
 
 public class Segundo {
+    int Arreglo2[] = new int[5];
+    void crearNuevoArreglo(int arr[]){
+
+        for (int i = 0; i < Arreglo2.length; i++){
+            Arreglo2[i] = arr[i];
+        }
+    }
     void mostrarTabla(int val){
-        System.out.println(" tabla del " + val);
-        for (int i = val; i<= val; i++){
+        System.out.println(" tabla del " + Arreglo2[val]);
+        for (int i = Arreglo2[val]; i<= Arreglo2[val]; i++){
             for (int j = 1; j <= 10; j++){
 
                 System.out.println(i + " * " + j + " = " + i * j);
--- Tercero.java
+++ Tercero.java
@@ -1,10 +1,18 @@
 package examen;
 
 public class Tercero {
+
+    int Arreglo3[] = new int[5];
+
+    void crearArreglo(int arr[]){
+        for (int i = 0; i < Arreglo3.length; i++){
+            Arreglo3[i] = arr[i];
+        }
+    }
     void mostrarTablas(){
-        for (int i = 1; i<= 10; i++){
+        for (int i = 1; i<= Arreglo3.length; i++){
             System.out.println("----------------------------------");
-            for (int j = 1; j <= 10; j++){
+            for (int j = 1; j <= Arreglo3.length; j++){
 
                 System.out.println(i + " * " + j + " = " + i * j);
 
